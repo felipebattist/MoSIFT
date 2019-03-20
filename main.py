62aa6beadb689c12e5004d358302f3f4fbb05cfd
arnaldovitor <arnaldovitorbarros@gmail.com>
2019-03-20 15:46:33 -0300

    Criação dos arquivos csv

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import kmeans_module as km
 import sift_module as sm
 import numpy as np
+import pandas as pd
 
 data = sm.gen_data_set()
-data_array = np.asarray(data)
-
 video = sm.gen_video_3d_points(r'C:\Users\Arnaldo\Desktop\MoSIFT\test\002.wmv')
-video_array = np.asarray(video)
 
-kmeans = km.clustering(data_array,2)
+dftrain = pd.DataFrame(data)
+dftrain.to_csv('train.csv',index=False)
 
-#testes
-histogram = km.gen_feature_vector(data_array,kmeans)
-histogram2 = km.gen_feature_vector(video_array,kmeans)
+dftest = pd.DataFrame(video)
+dftest.to_csv('test.csv',index=False)
+
+kmeans = km.clustering(data,2)
+
+histogram_ds = km.gen_feature_vector(data,kmeans)
+histogram_video = km.gen_feature_vector(video,kmeans)
+
